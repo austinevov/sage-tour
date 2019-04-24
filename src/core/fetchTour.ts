9513c66d6aaeee492ee788651f5d52ad4fb3fc16
Austin McKenna <austin@evolutionv.com>
2019-04-23 22:18:28 -0400

    Add minimap

---
+++ src/core/fetchTour.ts
@@ -0,0 +1,88 @@
+import axios from 'axios';
+import { PanoramaGraphNode } from './types';
+
+export interface TourData {
+  panoramas: PanoramaGraphNode[];
+  floorplans: {
+    id: number;
+    path: string;
+    floor: number;
+  }[];
+  token: string;
+  viewBoxX: number;
+  viewBoxY: number;
+}
+
+function rotateVector(input: number[], theta: number): number[] {
+  const [vx, vy] = input;
+
+  const x = vx * Math.cos(theta) - Math.sin(theta) * vy;
+  const y = vx * Math.sin(theta) + Math.cos(theta) * vy;
+
+  return [x, y];
+}
+
+export default (tourToken: string): Promise<TourData> => {
+  return new Promise(resolve => {
+    axios
+      .get('https://sagetourstudio.live/api/fetch-tour', {
+        params: { tourToken }
+      })
+      .then(response => {
+        const floorplans = response.data.floorplans.map(fp => {
+          return {
+            id: parseInt(fp.id),
+            path: fp.assetOriginal,
+            floor: parseInt(fp.floor),
+            theta: parseFloat(fp.theta)
+          };
+        });
+
+        const fpByFloor = (floor: number) => {
+          return floorplans.filter(fp => fp.floor === floor)[0];
+        };
+
+        const panoramaGraph: PanoramaGraphNode[] = response.data.panoramas.map(
+          panorama => {
+            const floor = parseInt(panorama.floor);
+            const theta = fpByFloor(floor).theta;
+            console.log('THETA:', theta);
+            const id = parseInt(panorama.id);
+
+            const [vx, vy] = rotateVector(
+              [panorama.position.x, panorama.position.z],
+              theta
+            );
+
+            const position = [vx, panorama.position.y, vy];
+            const floorplanPosition = [
+              panorama.position.x,
+              panorama.position.y,
+              panorama.position.z
+            ];
+            const edges = panorama.neighbors.map(n => parseInt(n));
+            const name = panorama.label;
+            return {
+              floor,
+              id,
+              position,
+              edges,
+              name,
+              floorplanPosition
+            };
+          }
+        );
+
+        let { viewBoxX, viewBoxY } = response.data;
+        viewBoxX = parseInt(viewBoxX);
+        viewBoxY = parseInt(viewBoxY);
+        resolve({
+          panoramas: panoramaGraph,
+          floorplans,
+          token: tourToken,
+          viewBoxX,
+          viewBoxY
+        });
+      });
+  });
+};
---
+++ src/core/renderer/shader/cubemap-fs.glsl
@@ -0,0 +1,12 @@
+precision mediump float;
+
+varying vec3 v_position;
+
+uniform samplerCube u_cubemap;
+
+void main()	
+{
+  vec3 col = textureCube(u_cubemap, vec3(v_position.x, v_position.y, v_position.z)).rgb;
+
+  gl_FragColor = vec4(col, 1.0);
+}
